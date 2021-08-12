4f480e869a5359841fb397d99cfe541261bd0a70
Ivan and Evan <64557487+ivzap@users.noreply.github.com>
2021-08-11 23:39:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import time
 
 # Description: gets historical data after X and before X
 def get_reddit_histo(after, before, subreddit, limit):
+	# convert string times into datetimes/epoch
 	after = int(dt.datetime.strptime(after, '%Y-%m-%d %H:%M:%S').timestamp())
 	before = int(dt.datetime.strptime(before, '%Y-%m-%d %H:%M:%S').timestamp())
-
+	# get data from PushShiftAPI
 	api = PushshiftAPI()
 	data = list(api.search_submissions(after=after,
 							before=before,
@@ -20,7 +21,9 @@ def get_reddit_histo(after, before, subreddit, limit):
 
 # Description: gets recent X mins of data from reddit
 def get_reddit_recent(minutes):
+	# convert to datetime/epoch
 	after=int((dt.datetime.now() - dt.timedelta(minutes=minutes)).timestamp())
+	# get data from PushShiftAPI
 	data = list(api.search_submissions(after=after,
                             subreddit=subreddit,
                             filter=['url','author', 'title', 'subreddit'],
